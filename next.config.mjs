74c50c292d70d6c2022cbafe1c377928b4eba8d1
ashar khan <asharkhan1258@gmail.com>
2024-03-18 21:59:30 +0500

    onboarding process completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+      serverActions: {}, // Set to an empty object
+      serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "img.clerk.com",
+          },
+          {
+              protocol: "https",
+              hostname: "images.clerk.dev",
+          },
+          {
+              protocol: "https",
+              hostname: "uploadthing.com",
+          },
+          {
+              protocol: "https",
+              hostname: "placehold.co",
+          },
+      ],
+      // Remove the 'typescript' key, as it is not recognized
+    },
+};
 
 export default nextConfig;
