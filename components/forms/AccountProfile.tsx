62b32c7adda5db447c8f666d16f072f19f019a5b
ashar khan <asharkhan1258@gmail.com>
2024-03-19 18:54:22 +0500

    completed backend and working on threads section

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,7 +19,9 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
-import {useUploadThing} from '@/lib/uploadthing'
+import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
     user: {
@@ -29,12 +31,15 @@ interface Props {
         name: string;
         bio: string;
         image: string;
+        // path: string;
     };
     btnTitle: string;
 }
 const AccountProfile = ({ user, btnTitle }: Props) => {
-    const [ files, setFiles] = useState<File[]>([])
-    const {startUpload} = useUploadThing("media");
+    const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -44,13 +49,13 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             bio: user?.bio || "",
         }
     })
-    const handleImage= (e: ChangeEvent<HTMLInputElement>, fieldChange: (value:string) => void ) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
         const fileReader = new FileReader();
-        if(e.target.files && e.target.files.length > 0){
+        if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
             setFiles(Array.from(e.target.files));
-            if(!file.type.includes('image')) return;
+            if (!file.type.includes('image')) return;
             fileReader.onload = async (event) => {
                 const imageDataUrl = event.target?.result?.toString() || '';
                 fieldChange(imageDataUrl)
@@ -61,11 +66,26 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
         const hasImageChanged = isBase64Image(blob);
-        if (hasImageChanged){
+        if (hasImageChanged) {
             const imgRes = await startUpload(files)
-            if(imgRes && imgRes[0].url){
+            if (imgRes && imgRes[0].url) {
                 values.profile_photo = imgRes[0].url;
             }
+        }
+        await updateUser(
+            {
+                userId: user.id,
+                username: values.username,
+                name: values.name,
+                bio: values.bio,
+                image: values.profile_photo,
+                path: pathname
+            }
+        );
+        if (pathname === '/profile/edit') {
+            router.back();
+        } else {
+            router.push('/');
         }
     }
     return (
@@ -79,24 +99,25 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                         <FormItem className="flex items-center gap-4">
                             <FormLabel className="account-form_image-label">
                                 {field.value ? (
-                                <Image src={field.value} alt="profile photo" width={96} height={96} priority className="rounded-full object-contain" />
+                                    <Image src={field.value} alt="profile photo" width={96} height={96} priority className="rounded-full object-contain" />
                                 ) : (
-                                <Image src="/assets/profile.svg" alt="profile photo" width={24} height={24} className="object-contain" />
+                                    <Image src="/assets/profile.svg" alt="profile photo" width={24} height={24} className="object-contain" />
                                 )}
                             </FormLabel>
                             <FormControl className="flex-1 text-base-semibold text-gray-200">
                                 <Input
-                                type="file"
-                                accept="image/*"
-                                placeholder="upload a photo"
-                                className="account-form_image-input"
-                                onChange={(e) => handleImage(e, field.onChange)}
-                                 />
+                                    type="file"
+                                    accept="image/*"
+                                    placeholder="upload a photo"
+                                    className="account-form_image-input"
+                                    onChange={(e) => handleImage(e, field.onChange)}
+                                />
                             </FormControl>
+                            <FormMessage/>
                         </FormItem>
                     )}
                 />
-                 <FormField
+                <FormField
                     control={form.control}
                     name="name"
                     render={({ field }) => (
@@ -106,29 +127,31 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                             </FormLabel>
                             <FormControl>
                                 <Input
-                                type="text"
-                                {...field}
-                                className="account-form_input no-focus"
-                                 />
+                                    type="text"
+                                    {...field}
+                                    className="account-form_input no-focus"
+                                />
                             </FormControl>
+                            <FormMessage/>
                         </FormItem>
                     )}
                 />
-                  <FormField
+                <FormField
                     control={form.control}
                     name="username"
                     render={({ field }) => (
-                       <FormItem className="flex flex-col w-full gap-3">
+                        <FormItem className="flex flex-col w-full gap-3">
                             <FormLabel className="text-base-semibold text-light-2">
                                 username
                             </FormLabel>
                             <FormControl>
                                 <Input
-                                type="text"
-                                {...field}
-                                className="account-form_input no-focus"
-                                 />
+                                    type="text"
+                                    {...field}
+                                    className="account-form_input no-focus"
+                                />
                             </FormControl>
+                            <FormMessage/>
                         </FormItem>
                     )}
                 />
@@ -136,17 +159,18 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     control={form.control}
                     name="bio"
                     render={({ field }) => (
-                       <FormItem className="flex flex-col gap-3 w-full">
+                        <FormItem className="flex flex-col gap-3 w-full">
                             <FormLabel className="text-base-semibold text-light-2">
                                 Bio
                             </FormLabel>
                             <FormControl>
                                 <Textarea
-                                rows={10}
-                                className="account-form_input no-focus"
-                                {...field}
-                                 />
+                                    rows={10}
+                                    className="account-form_input no-focus"
+                                    {...field}
+                                />
                             </FormControl>
+                            <FormMessage/>
                         </FormItem>
                     )}
                 />
